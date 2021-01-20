eed184ddc323691edb19e7efdb4c78944d1de26e
c-wegner <cwegner@wegnerlawfirm.com>
2021-01-20 11:50:20 -0500

    a

--- marketing/src/App.tsx
+++ marketing/src/App.tsx
@@ -1,7 +1,8 @@
 import React, { Fragment } from 'react';
 //import logo from './logo.svg';
-import { Global, NavProvider, Section, AnimateLeftToRight, Bio, Animater } from './custom';
+import { Global, NavProvider, Section} from './custom';
 import {Hero} from './components';
+
 
 
 
@@ -12,23 +13,13 @@ function App() {
       <NavProvider>
         <Hero />
         <Section id='About Us'  backgroundColor='lightblue'>
-          <Bio.Img/>
+   
         </Section>
 
         <Section id='Practices'>
-         <AnimateLeftToRight
-          start={550}
-          move={375}
-          fadeFrom={0}
-          fadeTo={1}
-          marginTop='10vh'
-          marginBottom='10vh'
-         >
-           This is a test of the animation
-         </AnimateLeftToRight>
         </Section>
         <Section id='Industries'>
-          1r1r
+
         </Section>
       </NavProvider>
     </Fragment>
--- marketing/src/custom/index.tsx
+++ marketing/src/custom/index.tsx
@@ -2,8 +2,5 @@ import {Global, screen, scrollTo} from './global/styles';
 import {NavProvider} from './nav/nav.context';
 import {Section, SectionContext} from './nav/nav.section';
 import {useObserver} from './global/observer';
-import {AnimateLeftToRight} from './animation/animation.container';
-import * as Bio from './about/about.img';
-import * as Animater from './animation/animation.container';
 
-export {Global, screen, NavProvider, Section, scrollTo, useObserver, SectionContext, AnimateLeftToRight, Bio, Animater}
+export {Global, screen, NavProvider, Section, scrollTo, useObserver, SectionContext}
