353384f3b64be46db129a96b62787c5f524ff2d2
c-wegner <cwegner@wegnerlawfirm.com>
2021-02-07 11:36:31 -0500

    Feb 7 2021

--- marketing/src/_custom/nav/nav.section.tsx
+++ marketing/src/_custom/nav/nav.section.tsx
@@ -2,6 +2,13 @@ import React, {createContext, useContext, useRef, useEffect} from 'react';
 import {NavContext, INavContext} from './nav.context';
 import {scrollTo, Stage, useObserver, Models} from '../';
 
+export class ISectionContext{
+    onScreen: number;
+    section: Models.Section;
+}
+
+export const SectionContext = createContext(null)
+
 export const Section =({children, id})=>{
     const navContext: INavContext = useContext(NavContext)
     let ref= useRef()
